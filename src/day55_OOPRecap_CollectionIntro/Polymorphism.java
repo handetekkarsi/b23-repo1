9d0cd7bb569b6a03a70dafa0974bacd849cf51c2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-16 14:26:31 -0400

    day55

--- src/day55_OOPRecap_CollectionIntro/Polymorphism.java
+++ src/day55_OOPRecap_CollectionIntro/Polymorphism.java
@@ -2,6 +2,7 @@ package day55_OOPRecap_CollectionIntro;
 
 import day37_CustomClass.Dog;
 import day51_Abstraction.shapeTask.Circle;
+import day51_Abstraction.shapeTask.Rectangle;
 import day51_Abstraction.shapeTask.Shape;
 
 public class Polymorphism {
@@ -12,12 +13,20 @@ public class Polymorphism {
        // obj.play();
         (  (Dog) obj ).play(); // down casting
 
+        System.out.println( obj instanceof Dog);
+
         System.out.println("--------------------------------------");
 
         Shape obj2 =  new Circle(3);
 
         System.out.println( obj2.area() );
         System.out.println( ((Circle) obj2).getR() );
+
+        System.out.println(obj2 instanceof Rectangle);
+
+
+       // System.out.println( ((Rectangle)obj2).getW() );
+
 
 
 
