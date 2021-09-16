7cb6368d69756c6b9a98af09ff31d99c09bb0f86
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-16 19:30:59 -0400

    day55

--- src/day55_OOPRecap_CollectionIntro/CollectionIntro.java
+++ src/day55_OOPRecap_CollectionIntro/CollectionIntro.java
@@ -25,24 +25,46 @@ public class CollectionIntro {
 
         List<Integer> list = new ArrayList<>();
         list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        list.remove(10);
 
         System.out.println("list = " + list);
         System.out.println(list.get(4));
 
         Set<Integer> set = new HashSet<>();
         set.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        set.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        set.remove(10);
 
         System.out.println("set = " + set);
      //   System.out.println(set.get(4));
 
         Queue<Integer> queue = new PriorityQueue<>() ;
         queue.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        queue.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        queue.remove(10);
 
         System.out.println("queue = " + queue);
     //    System.out.println(queue.get(4));
 
 
+        System.out.println("-----------------------------------------");
+/*
+        for(int i =0; i <= list.size()-1; i++){
+            if(list.get(i) < 7){
+                list.remove(i);
+            }
+        }
 
+ */
+
+        list.removeIf( p -> p < 7);
+
+
+        System.out.println(list);
 
 
 
