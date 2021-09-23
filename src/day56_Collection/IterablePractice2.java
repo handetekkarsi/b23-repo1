1888abdfc201331d075ea9a7a6b6e5498d3906cd
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-23 15:53:27 -0400

    day56

--- src/day56_Collection/IterablePractice2.java
+++ src/day56_Collection/IterablePractice2.java
@@ -1,9 +1,6 @@
 package day56_Collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class IterablePractice2 {
     public static void main(String[] args) {
@@ -34,6 +31,40 @@ public class IterablePractice2 {
         names2.removeIf( p -> p.equalsIgnoreCase("ahmed") );
 
         System.out.println(names2);
+
+
+        System.out.println("--------------------------------------------------");
+
+        Set<Integer> set = new HashSet<>();
+        set.addAll(Arrays.asList(10,9, 11, 1,2,3,4,5,6,7,12,13,14,15,16,18,17,25,20));
+
+        // remove all the elemnts that are less than 17:
+
+        for( Iterator<Integer> i = set.iterator();  i.hasNext(); ){
+            if(i.next() < 17){
+                i.remove();
+            }
+        }
+
+        System.out.println(set);
+
+        System.out.println("------------------------------------------------------------------");
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.addAll(Arrays.asList(10,9, 11, 1,2,3,4,5,6,7,12,13,14,15,16,18,17,25,20));
+
+        set2.removeIf( p -> p < 17);
+
+        System.out.println(set2);
+
+        System.out.println("--------------------------------------------------------");
+
+
+        for(int i =1, j =10, k = 100; i <= 10;  i++, j+= 10, k+= 100){
+            System.out.println(i+" : "+j+" : "+k);
+        }
+
+
 
 
 
