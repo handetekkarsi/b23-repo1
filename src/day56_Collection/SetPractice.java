f66d87655f24d0424a4cd24f1db377997a7406d5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-23 12:38:00 -0400

    day56

--- src/day56_Collection/SetPractice.java
+++ src/day56_Collection/SetPractice.java
@@ -9,19 +9,45 @@ public class SetPractice {
         String[] arr = {"Wooden Spoon", "Book", "Pen", "Phone", "Wooden Spoon",
                 "Wooden Spoon", "Wooden Spoon", "Wooden Spoon", "Milk", "Eggs", "Coke", "Paper towels"};
 
-        Set<String> items1 =new HashSet<>( Arrays.asList(arr) );
+        Set<String> items1 =new HashSet<>(  );
+        items1.addAll( Arrays.asList(arr) );
         items1.add( null );
 
-        Set<String> items2 = new LinkedHashSet<>( Arrays.asList(arr) );
+        Set<String> items2 = new LinkedHashSet<>(  );
+        items2.addAll( Arrays.asList(arr) );
         items2.add( null );
 
         Set<String> items3 = new TreeSet<>( Arrays.asList(arr) );
-    //   items3.add( null ); // tree set does not accept null
+        items3.addAll( Arrays.asList(arr) );
+     //   items3.add( null ); // tree set does not accept null
+
 
         System.out.println("items1 = " + items1);
         System.out.println("items2 = " + items2);
         System.out.println("items3 = " + items3);
 
+
+      //  String str = null;
+       // str.equals("Cybertek");
+
+        System.out.println("----------------------------------------------------");
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5));
+
+          list = new ArrayList<>( new LinkedHashSet<Integer>(list) );
+
+        System.out.println(list);
+
+        System.out.println("-----------------------------------------------------");
+
+        String str = "abcabcabcabc";
+      //  str =  new LinkedHashSet<>( Arrays.asList( str.split("")) ) .toString().substring(1).replace(", ", "").replace("]", "");
+        str =  new LinkedHashSet<>( Arrays.asList( str.split("")) ) .toString();
+
+        System.out.println(str);
+
+
+        System.out.println("--------------------------------------------");
 
 
     }
