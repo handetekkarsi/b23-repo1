e2694ebf112862afd55831f778bbdb246e7415d5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-23 11:16:34 -0400

    day56

---
+++ src/day56_Collection/ListPractice.java
@@ -0,0 +1,29 @@
+package day56_Collection;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class ListPractice {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.size();
+
+        Vector<Integer> vector = new Vector<>();
+        vector.size();
+
+
+
+    }
+
+    public void method1(){
+
+    }
+
+    public synchronized void method2(){
+
+    }
+
+
+}
