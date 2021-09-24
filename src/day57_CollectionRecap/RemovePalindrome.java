b2c68de5a24578eb0bdcb6e87b9f04d87c65235b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-24 10:13:45 -0400

    day57

---
+++ src/day57_CollectionRecap/RemovePalindrome.java
@@ -0,0 +1,22 @@
+package day57_CollectionRecap;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RemovePalindrome {
+
+    public static void main(String[] args) {
+
+        List<String> list = new LinkedList<>();
+        list.addAll(Arrays.asList("Java", "Python", "Level", "Kayak", "Cybertek", "Zaman", "Ana", "Batch23"));
+
+
+    }
+
+}
+
+/*
+1. write a program that can remove the palindrome string from a list of String
+			DO NOT use Lambda expressions
+ */
