169c30ee37f54f28c4f6b945afdcd3b862eaf5a3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-30 14:51:58 -0400

    day58

---
+++ src/day58_Maps/ListOfMaps2.java
@@ -0,0 +1,29 @@
+package day58_Maps;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class ListOfMaps2 {
+
+    public static void main(String[] args) {
+        String[] familyMember = {"John","Kevin","Lik"};
+        LocalDate[] DOBFamilyMember = {LocalDate.of(1992,01,02),LocalDate.of(1993,01,03),LocalDate.of(1994,01,04)};
+
+        String[] classMates = {"Jerry","Karim","Merk","Clark"};
+        LocalDate[] DOBClassMates = {LocalDate.of(1995,01,05),LocalDate.of(1995,01,06),LocalDate.of(1996,01,07),LocalDate.of(1997,01,8)};
+
+        String[] friends = {"Erik","Kahri","Morh","Jonny"};
+        LocalDate[] DOBFriends = {LocalDate.of(1997,01,05),LocalDate.of(1995,01,06),LocalDate.of(1996,01,07),LocalDate.of(1997,01,8)};
+
+
+        List<Map<String, LocalDate>> listOfMap = new ArrayList<>();
+        listOfMap.addAll(Arrays.asList(
+                new HashMap<>(), // family members & their DoB
+                new HashMap<>(), // class mates & their DoB
+                new HashMap<>() /// friends & their DoB
+        ));
+
+
+
+    }
+}
