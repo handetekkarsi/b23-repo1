c2331422c553795be75f62fc67fd849c96d8257c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-30 15:55:23 -0400

    day58

--- src/day58_Maps/TestEnum.java
+++ src/day58_Maps/TestEnum.java
@@ -4,6 +4,31 @@ public class TestEnum {
 
     public static void main(String[] args) {
 
+     //   String browser = "Cydeo";
+
+        Browser browser = Browser.chrome;
+
+        switch (browser){
+
+            case firefox:
+                System.out.println("Firefox driver is selected");
+                break;
+
+            case edge:
+                System.out.println("Edge driver is selected");
+                break;
+
+            case  safari:
+                System.out.println("Safari driver is selected");
+                break;
+
+            case  opera:
+                System.out.println("Opera driver is selected");
+                break;
+
+            default:
+                System.out.println("Chrome driver is selected");
+        }
 
 
     }
