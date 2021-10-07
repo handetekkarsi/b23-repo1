e2a9911a251aaf9c5d79ea54ad46133d9db0b8ea
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-10-07 14:41:58 -0400

    day60

--- src/day60_Functional_Interface/BuildInFunctionalInterface2.java
+++ src/day60_Functional_Interface/BuildInFunctionalInterface2.java
@@ -1,8 +1,7 @@
 package day60_Functional_Interface;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class BuildInFunctionalInterface2 {
@@ -41,6 +40,33 @@ public class BuildInFunctionalInterface2 {
     ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
         maxNumFromArrayList.apply(list);
 
+
+    System.out.println("---------------------------------------------------------");
+        // forEach
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("John", 95000);
+        map.put("Jimmy", 100000);
+        map.put("Josh", 105000);
+        map.put("Daniel", 120000);
+        map.put("Aaron", 115000);
+
+        map.forEach( (k,v) ->  System.out.println(k+" : "+v) );
+
+        System.out.println("--------------------------------------------------");
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+
+        System.out.println("-----------------------------------------------");
+        // create a function that can repeat the given string for the given number of times
+        BiConsumer<String, Integer> printMultipleTimes = (s, n) -> {
+            for (int i = 0; i < n; i++) {
+                System.out.println(s);
+            }
+        };
+
+        printMultipleTimes.accept("Java", 10);
 
 
     }
