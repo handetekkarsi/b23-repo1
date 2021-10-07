492babac71af28571fcef7dd6738cefc59897639
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-10-07 11:21:57 -0400

    day60

--- src/day60_Functional_Interface/FunctionalInterfaceTest.java
+++ src/day60_Functional_Interface/FunctionalInterfaceTest.java
@@ -38,6 +38,24 @@ public class FunctionalInterfaceTest {
 
 
         System.out.println("------------------------------------------------");
+        // create a function that can reverse a string
+        DataFunction<String> reverseString = (s) ->  new StringBuilder(s).reverse().toString();
+        String r1 = reverseString.accept("Wooden Spoon");
+
+        System.out.println("r1 = " + r1);
+
+        // create a function that can return the first three characters of the string
+        DataFunction<String> firstThreeChars = s -> s.substring(0,3);
+
+        String r2 = firstThreeChars.accept("Wooden Spoon");
+        System.out.println("r2 = " + r2);
+
+        //create a function that can return the cube of an integer
+        DataFunction<Integer> cube = (n) -> n*n*n ;
+        int r3 = cube.accept(5);
+
+        System.out.println("r3 = " + r3);
+
 
 
 
