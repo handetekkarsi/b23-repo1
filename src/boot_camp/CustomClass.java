48d71ce283397c6848c74080e0da1dca35929201
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-23 12:13:55 -0500

    Java Boot camp

---
+++ src/boot_camp/CustomClass.java
@@ -0,0 +1,31 @@
+package boot_camp;
+
+class Student{
+
+    public String name;
+
+    public static String schoolName = "Cydeo School";
+
+
+}
+
+
+public class CustomClass {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        student1.name = "David";
+
+        Student student2 = new Student();
+        student2.name = "Aaron";
+
+        Student student3 = new Student();
+        student3.name = "Shay";
+
+
+
+
+    }
+
+}
