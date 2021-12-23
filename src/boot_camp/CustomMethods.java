37ab38be1a7cf9129d5767255d7c72c6978dd027
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-23 11:30:58 -0500

    Java Boot camp

--- src/boot_camp/CustomMethods.java
+++ src/boot_camp/CustomMethods.java
@@ -1,4 +1,33 @@
 package boot_camp;
 
 public class CustomMethods {
+
+
+    public static void main(String[] args) {
+
+        String str = "Wooden Spoon";
+
+        String result = reverse(str);
+
+        System.out.println(result);
+
+
+    }
+
+
+
+    public static String reverse(String str){
+
+        String reverse = "";
+
+        for( int i = str.length()-1;  i >= 0; i--  ){
+
+            reverse += str.charAt(i);
+
+        }
+
+        return reverse;
+    }
+
+
 }
--- src/boot_camp/ForLoop.java
+++ src/boot_camp/ForLoop.java
@@ -5,6 +5,7 @@ public class ForLoop {
     public static void main(String[] args) {
 
         String str = "Cydeo";
+
         String reverse = "";
 
 
