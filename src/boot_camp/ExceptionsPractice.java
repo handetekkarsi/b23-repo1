0649140f67253d25c52368b9af10ac7d13137b7b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-23 16:11:33 -0500

    Java Boot camp

---
+++ src/boot_camp/ExceptionsPractice.java
@@ -0,0 +1,22 @@
+package boot_camp;
+
+import java.io.FileInputStream;
+
+public class ExceptionsPractice {
+
+    public static void main(String[] args) {
+
+       // FileInputStream file = new FileInputStream(""); // checked exception
+
+
+        int[] arr = {1,2,3};
+
+   //    System.out.println( arr[2000] ); // unchecked exception
+
+   //     new ExceptionsPractice().finalize();
+
+
+    }
+
+
+}
